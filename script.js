6d373f1267e5ea81ca828f66c66d69891dcc4a99
matheuefranco <matheusefranco@gmail.com>
2024-10-02 20:33:35 -0300

    2024

--- script.js
+++ script.js
@@ -4,51 +4,18 @@ document.getElementById("donateBtn").addEventListener("click", function () {
     alert("Redirecionando para a página de doação...");
 });
 
-function loadCSV() {
-    // Caminho para o arquivo CSV
-    const csvFilePath = 'actions.csv';
-    console.log("loadCSV");
-    // Use a biblioteca PapaParse para ler o CSV
-    Papa.parse(csvFilePath, {
-        header: true,
-        download: true,
-        dynamicTyping: true,
-        complete: function (results) {
-            const actions = results.data;
-            const tableBody = document.querySelector('#actions-table tbody');
 
-            // Limpe qualquer conteúdo existente na tabela
-            tableBody.innerHTML = '';
-
-            // Preencha a tabela com os dados do CSV
-            actions.forEach(function (action) {
-                const row = document.createElement('tr');
-                const nameCell = document.createElement('td');
-                const descriptionCell = document.createElement('td');
-
-                nameCell.textContent = action.Ação;
-                descriptionCell.textContent = action.Descrição;
-
-                row.appendChild(nameCell);
-                row.appendChild(descriptionCell);
-                tableBody.appendChild(row);
-            });
-        }
-    });
-}
-
-//window.addEventListener('load', loadCSV);
 
 // Função para carregar e exibir dados da planilha do Google Sheets
 function loadGoogleSheetData() {
     // ID da planilha do Google Sheets
-    const spreadsheetId = '1XuWvCCAklYjlNbOUiXn198HdFskEtzYH0AYvVehQrGc';
+    const spreadsheetId = '16BqeIGtsO47jb6yABaJyLEiE29qF6kPan6ZHlg6TsKQ';
     // ID da planilha dentro do documento (geralmente 0 para a primeira planilha)
     const sheetId = 0;
 
     gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: spreadsheetId,
-        range: 'info' // Substitua pelo nome da aba que você deseja ler
+        range: 'acoes' // Substitua pelo nome da aba que você deseja ler
     }).then(function(response) {
         const data = response.result.values;
         const tableBody = document.querySelector('#actions-table tbody');
@@ -75,7 +42,7 @@ function loadGoogleSheetData() {
 // Função para inicializar a API do Google Sheets
 function initGoogleSheetsApi() {
     gapi.client.init({
-        apiKey: 'AIzaSyDRRx2N7H9kW0iAT5pPaBnqlG5MNk9oJQA',
+        apiKey: 'AIzaSyDunp5UoDY4LeKYFo92ByH9MgIYiVehBaI',
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(function() {
         loadGoogleSheetData();
